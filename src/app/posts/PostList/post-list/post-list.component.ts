0ff92f20acec0476eaf758efab5cd750191a7375
Matheus Duarte <mfduar8766@gmail.com>
2018-11-01 20:26:47 -0400

    Added services to remove all the passing of methods from component to component.

--- src/app/posts/PostList/post-list/post-list.component.ts
+++ src/app/posts/PostList/post-list/post-list.component.ts
@@ -1,18 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Post } from '../../../../Models/post.model';
+import { Component, OnInit, OnDestroy  } from '@angular/core';
+ import { PostService } from 'src/Services/post.service';
+import { Post } from 'src/Models/post.model';
+import { Subscription  } from 'rxjs';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
   // Makes posts bindable from outside IE: Accept outputs as input
-  @Input() posts: Post[] = [];
 
-  constructor() { }
+  /* Using the public keyword: Automatically creates new property in component and sets the value*/
+
+  posts: Post[] = [];
+  private subscribedPosts: Subscription;
+
+  constructor( public postService: PostService) { }
 
   ngOnInit() {
+    this.posts = this.postService.getPosts();
+   this.subscribedPosts = this.postService.getUpdatedPostListener().
+    subscribe((data: Post[]) => {
+      this.posts = data;
+    });
+  }
+
+  ngOnDestroy() {
+     this.subscribedPosts.unsubscribe();
   }
 
 }
